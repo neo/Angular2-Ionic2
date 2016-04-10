9aaac4aa741e50b64fc72d2a87cd525222bf719a
Neo <neolwc@gmail.com>
2016-04-10 16:40:08 -0400

    Import Observable.throw

--- app/github.service.ts
+++ app/github.service.ts
@@ -3,6 +3,7 @@ import {Http, Response} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class GitHubService {
